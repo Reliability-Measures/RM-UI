9f0af5831bd0ce36fbf4732ceeb7e1b565f81d74
ThahaShahzad <mw3boy8thstrox@gmail.com>
2020-06-12 01:07:37 -0500

    Quiz create page  Moved selected items to 3rd box  Added keyword search for getting questions Small Warning Fixes

--- src/Components/Quiz_create_page/submit_form.js
+++ src/Components/Quiz_create_page/submit_form.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useForm, FormContext } from 'react-hook-form'
-import { Form, Button, Col, Row } from 'react-bootstrap'
+import { Form, Button, Col, Row, Dropdown } from 'react-bootstrap'
 import MyInput from '../Quiz_question_page/Reuse_components/my_input'
 import QuizConfirmModal from './quiz_confirm_modal'
 import { useSelector } from 'react-redux'
@@ -15,6 +15,20 @@ function SubmitForm() {
   const [modalShow, setModalShow] = React.useState(false)
   const items_recived = useSelector((state) => state.quiz_question.item_get_received)
   const is_login = useSelector((state) => state.google_json.isLogin)
+  const state_ids = useSelector((state) => state.quiz_question.items_selected_id)
+  const state_texts = useSelector((state) => state.quiz_question.items_selected_text)
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a
+      href='/#'
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault()
+        onClick(e)
+      }}>
+      {children}
+      &#x25bc;
+    </a>
+  ))
   const quiz_create = useForm({
     defaultValues: {
       quiz_name: '',
@@ -30,6 +44,23 @@ function SubmitForm() {
   return (
     <>
       <FormContext {...quiz_create}>
+        <br />
+        <Row>
+          <Col>Total Items Selected: {state_ids.length}</Col>
+          <Col>
+            <Dropdown>
+              <Dropdown.Toggle as={CustomToggle} id='dropdown-custom-components'>
+                Selected items
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                {state_texts.map((val, index) => (
+                  <Dropdown.Item key={index}>{val}</Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+        </Row>
+        <br />
         <h5>
           <i className='fas fa-star-of-life text-danger fa-xs'></i> Quiz name
           {errors.quiz_name && <p className='text-danger'>Required</p>}
